5522c6e762b8dde5f81140855a82559bb61905f2
Tobias Norton <tnorton20@yahoo.com>
2018-11-14 15:46:47 -0500

    Values change dynamically with sliders

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hueValue: 25,
+      saturationValue: 50,
+      lightnessValue: 10
+    }
+  }
+
+  changeHueValue = event => {
+    this.setState({
+      hueValue: event.target.value
+    })
+  }
+
+  changeSaturationValue = event => {
+    this.setState({
+      saturationValue: event.target.value
+    })
+  }
+
+  changeLightnessValue = event => {
+    this.setState({
+      lightnessValue: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="box">
         <h1>HSL Color Picker</h1>
-        <div className="colors">
+        <div className="colors ">
           <label htmlFor="Hue">Hue</label>
-          <input name="Hue" type="range" />
+          <input
+            name="Hue"
+            type="range"
+            min="0"
+            max="360"
+            onChange={this.changeHueValue}
+            value={this.state.hueValue}
+          />
+
           <label htmlFor="Saturation">Saturation</label>
-          <input name="Saturation" type="range" />
+          <input
+            name="Saturation"
+            type="range"
+            min="0"
+            max="100"
+            onChange={this.changeSaturationValue}
+            value={this.state.saturationValue}
+          />
+
           <label htmlFor="Lightness">Lightness</label>
-          <input name="Lightness" type="range" />
+          <input
+            name="Lightness"
+            type="range"
+            min="0"
+            max="100"
+            onChange={this.changeLightnessValue}
+            value={this.state.lightnessValue}
+          />
         </div>
       </div>
     )
